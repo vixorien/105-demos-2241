fed75c2ea5024fb162ab0d6e8529135e2db36f6a
Chris Cascioli <chriscascioli@gmail.com>
2024-10-04 09:33:55 -0400

    Added OOP demo

---
+++ OOPDemo/Car.cs
@@ -0,0 +1,96 @@
+﻿// Chris Cascioli
+// 10/4/24
+// Example of a class
+
+namespace OOPDemo
+{
+	internal class Car
+	{
+		// Fields
+		private string make;
+		private string model;
+		private string color;
+		private double mileage;
+		private double milesPerGallon;
+		private double gas;
+		private const double GasTankMaximum = 10;
+
+		// Constructors
+		public Car(string make, string model, string color, double milesPerGallon)
+		{
+			this.make = make;
+			this.model = model;
+			this.color = color;
+			this.milesPerGallon = milesPerGallon;
+			mileage = 0;
+			gas = 0;
+		}
+
+		// Methods
+		public void FillGasTank()
+		{
+			gas = GasTankMaximum;
+			Console.WriteLine("Gas tank has been filled");
+		}
+
+		public void CheckGasTank()
+		{
+			Console.WriteLine($"You have {gas} gallons of gas left.");
+		}
+
+
+		public void Drive(double distance)
+		{
+			// Make sure we can do anything
+			if (distance <= 0)
+			{
+				Console.WriteLine("That was a short trip!");
+				return;
+			}
+			else if (gas == 0)
+			{
+				Console.WriteLine("You have no gas!");
+				return;
+			}
+
+			// How much gas will this require?
+			double gasRequired = distance / milesPerGallon;
+
+			// Do we have enough?
+			if (gasRequired > gas)
+			{
+				// Nope!  How far will we get?
+				double distBeforeEmpty = gas * milesPerGallon;
+				mileage += distBeforeEmpty;
+				gas = 0;
+
+				Console.WriteLine($"You don't have enough gas to drive {distance} miles.");
+				Console.WriteLine($"You drove {distBeforeEmpty} miles before running out of gas!");
+			}
+			else if (gasRequired == gas)
+			{
+				// We have EXACTLY enough gas
+				mileage += distance;
+				gas -= gasRequired;
+				Console.WriteLine($"You've driven {distance} miles, but now you're out of gas!");
+			}
+			else
+			{
+				// We have more than enough gas
+				mileage += distance;
+				gas -= gasRequired;
+				Console.WriteLine($"You've driven {distance} miles");
+				CheckGasTank();
+			}
+		}
+
+		public void Report()
+		{
+			Console.WriteLine($"You are driving a {color} {make} {model}");
+			Console.WriteLine($"It gets {milesPerGallon} mpg");
+			Console.WriteLine($"You have driven a total of {mileage} miles so far");
+			CheckGasTank();
+		}
+
+	}
+}
---
+++ OOPDemo/Program.cs
@@ -0,0 +1,55 @@
+﻿// Chris Cascioli
+// 10/4/24
+// Demo of object oriented programming
+
+namespace OOPDemo
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			// Create a car object
+			Car firstCar = new Car("VW", "Jetta", "Blue", 20);
+
+			// Test it out!
+			firstCar.Report();
+
+			// NOT THIS!!!
+			// Car.Report(); // Report is NOT STATIC
+
+			string input = null!;
+			while (input != "quit")
+			{
+				Console.WriteLine("\nWhat would you like to do? ['drive', 'fill tank', 'report', 'quit']");
+				Console.Write("> ");
+				input = Console.ReadLine()!.ToLower();
+
+				switch (input)
+				{
+					case "drive":
+						Console.Write("How far are you driving? ");
+						double distToDrive = double.Parse(Console.ReadLine()!);
+						firstCar.Drive(distToDrive);
+						break;
+
+					case "fill tank":
+						firstCar.FillGasTank();
+						break;
+
+					case "report":
+						firstCar.Report();
+						break;
+
+					case "quit":
+						Console.WriteLine("Thanks for driving!");
+						break;
+
+					default:
+						Console.WriteLine("That is not a valid command");
+						break;
+				}
+			}
+
+		}
+	}
+}
